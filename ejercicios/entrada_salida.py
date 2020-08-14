673b1add157aebca2518b840e82a451c48eecc80
andrea1807 <andreamelenje1807@gmail.co>
2020-08-14 18:37:31 -0500

    Commit3
    entradaS1

---
+++ ejercicios/entrada_salida.py
@@ -0,0 +1,5 @@
+nombre = input("Cual es tu nombre: ")
+edad = input("Cual es tu edad: ")
+fecha = input("Fecha de nacimiento: ")
+
+print(f"Bienvenido {nombre}, tu edad es {edad}, naciste el {fecha}")
