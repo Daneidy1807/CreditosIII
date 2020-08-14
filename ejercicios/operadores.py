dca02712c1d9ad482f01fd6f5a14ae657a6aec53
andrea1807 <andreamelenje1807@gmail.co>
2020-08-14 18:37:54 -0500

    Commit4
    Operadores1

---
+++ ejercicios/operadores.py
@@ -0,0 +1,14 @@
+num1 = int(input("ingresar el primer numero "))
+num2 = int(input("ingresar el segundo numero "))
+result = num1+num2
+
+print(f"El resultado es: {result}")
+
+#ejercicio
+
+print("EJERCICIO")
+
+dato1 = 24
+dato2 = 2
+result=dato1*dato2
+print(f"El resultado de la multiplicacion es: {result}")
